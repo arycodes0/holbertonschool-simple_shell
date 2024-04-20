7c139358e42cf9fdd9aa76683727a787537275c0
Ary <alvarezaryam@gmail.com>
2024-04-19 22:37:28 -0400

    Adding header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h> /* We use this for the getline */
+#include <stdlib.h> /* We use this for the exit */
+#include <unistd.h> /* We use this for the write, fork, execve, and isatty*/
+#include <string.h> /* We use this for the strtok*/
+
+/* PROTOTYPES */
+int main(int ac, char **av,  char **env);
+
+#endif
